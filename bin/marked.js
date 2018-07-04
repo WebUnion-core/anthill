771113fec1d630539e50e8710dafdda84774de1f
WJT20 <13415156317@163.com>
2018-07-04 19:02:04 +0800

    增加npm文件

---
+++ bin/marked.js
@@ -0,0 +1,20 @@
+let path = require('path');
+let fs = require('fs');
+let marked = require('marked');
+
+const srcPath = path.resolve(__dirname, './../WJT20');
+const allMdFiles = fs.readdirSync(srcPath);
+const jsonData = [];
+
+allMdFiles.forEach(function(item, index) {
+    if (item !== 'images') {
+        const fileCont = fs.readFileSync(srcPath + '/' + item, 'utf-8');
+        const resultCont = encodeURI(marked(fileCont));
+        jsonData.push({
+            name: item,
+            cont: resultCont
+        });
+    }
+});
+
+fs.writeFileSync(path.resolve(__dirname, './../markdown-data.json'), JSON.stringify(jsonData, null, 4), 'utf-8');
