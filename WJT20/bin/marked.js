a29c6cd6a825ee1f56d92d20b21c9aed2a61c70a
WeiJietao <13415156317@163.com>
2018-07-06 11:28:51 +0800

    更新npm配置

---
+++ WJT20/bin/marked.js
@@ -0,0 +1,57 @@
+let path = require('path');
+let fs = require('fs');
+let marked = require('marked');
+
+const srcPath = path.resolve(__dirname, './..');
+const dataPath = path.resolve(__dirname, './../database');
+const allMdFiles = fs.readdirSync(srcPath);
+const jsonData = [];
+const dataType = 'JSON';
+
+// 组装数据
+function readAllFiles() {
+    allMdFiles.forEach(function(item, index) {
+        const filePath = srcPath + '/' + item;
+
+        // isFile: 是否文件
+        // isDirectory: 是否目录
+        if (fs.statSync(filePath).isFile()) {
+            const fileCont = fs.readFileSync(filePath, 'utf-8');
+            const resultCont = encodeURI(marked(fileCont));
+
+            if (index % 50 === 0) {
+                jsonData.push([]);
+            }
+
+            jsonData[jsonData.length - 1].push({
+                name: item,
+                cont: resultCont
+            });
+        }
+    });
+}
+
+// 写入所有文件数据
+function writeAllDataFiles() {
+    jsonData.forEach(function(item, index) {
+        const hash = 'md-data' + (index + 1);
+        // const hash = (new Date()).valueOf();
+        const name = hash + '.' + dataType.toLowerCase();
+        writeDataFiles(name, JSON.stringify(item, null, 4));
+        console.log('output success: ' + name);
+    });
+}
+
+// 写入单个文件数据
+function writeDataFiles(fileName, content) {
+    fs.writeFileSync(
+        dataPath + '/' + fileName,
+        content,
+        'utf-8'
+    );
+}
+
+(function() {
+    readAllFiles();
+    writeAllDataFiles();
+})();
