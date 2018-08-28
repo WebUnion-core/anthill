afa3f8ebc174029fa8bfa71d77133d0fec176311
hongtao <836717428@qq.com>
2018-08-28 18:39:01 +0800

    链表

--- HT/javascript/设计模式/test.js
+++ HT/javascript/设计模式/test.js
@@ -1,57 +1,76 @@
-class Node {
-    constructor(el) {
-        this.element = el;
-        this.next = null;
+class HashTable {
+    constructor() {
+        this.table = new Array(137);
+    }
+
+    simpleHash(data) {
+        var total = 0;
+        for (var i = 0; i < data.length; ++i) {
+            total += data.charCodeAt(i);
+        }
+        console.log("Hash value: " + data + " -> " + total);
+        return total % this.table.length;
+    }
+
+    betterHash(string) {
+        const H = 37;
+        var total = 0;
+        for (var i = 0; i < string.length; ++i) {
+            total += H * total + string.charCodeAt(i);
+        }
+        total = total % this.table.length;
+        if (total < 0) {
+            total += this.table.length - 1;
+        }
+        return parseInt(total);
+    }
+    put(data) {
+        let pos = this.simpleHash(data);
+        this.table[pos] = data;
+    }
+
+    showDistro() {
+        for (let i in this.table) {
+            if (this.table[i] != undefined) {
+                console.log(`${i} : ${this.table[i]}`);
+            }
+        }
     }
 }
 
-let LinkedList = (() => {
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    let head = new Node("head");
-    return class LinkedList {
-        find(item) {
-            let currNode = head;
-            while (currNode.element != item) {
-                currNode = currNode.next;
-            }
-            return currNode;
+function genStuData(arr) {
+    for (var i = 0; i < arr.length; ++i) {
+        var num = "";
+        for (var j = 1; j <= 9; ++j) {
+            num += Math.floor(Math.random() * 10);
         }
-        findPrevious(item) {
-            let currNode = head;
-            while ((currNode.next != null) && (currNode.next.element != item)) {
-                currNode = currNode.next;
-            }
-            return currNode;
-        }
-        insert(el, item) {
-            let newNode = new Node(el);
-            let currNode = this.find(item);
-            newNode.next = currNode.next;
-            currNode.next = newNode;
-        }
-        remove(item) {
-            let currNode = this.findPrevious(item);
-            let preNode = this.find(item);
-            currNode.next = preNode.next;
-        }
-        display() {
-            let node = head;
-            while (node.next != null) {
-                console.log(node.next.element);
-                node = node.next;
-            }
-        }
+        num += getRandomInt(50, 100);
+        arr[i] = num;
     }
-})()
+}
+var numStudents = 10;
+var arrSize = 97;
+var idLen = 9;
+var students = new Array(numStudents);
 
-let aList =  new LinkedList();
+genStuData(students);
 
-aList.insert("1", "head")
-aList.insert("2", "1")
-aList.insert("3", "2")
+console.log("Student data: \n");
 
-aList.display(); // 1 2 3
+for (var i = 0; i < students.length; ++i) {
+    console.log(students[i].substring(0, 8) + " " +
+        students[i].substring(9));
+}
 
-aList.remove('3')
 
-aList.display();// 1 2
+console.log("\n\nData distribution: \n");
+
+var hTable = new HashTable();
+
+for (var i = 0; i < students.length; ++i) {
+    hTable.put(students[i]);
+}
