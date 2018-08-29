ec73acdca430105252859387a50b9b0021a1d808
hongtao <836717428@qq.com>
2018-08-29 14:32:32 +0800

    集合

--- HT/javascript/设计模式/test.js
+++ HT/javascript/设计模式/test.js
@@ -1,64 +1,29 @@
-class HashTable {
+class Set {
     constructor() {
-        this.table = new Array(137);
+        this.dataStore = [];
     }
+    add(data) {
+        if (this.dataStore.indexOf(data) < 0) {
+            this.dataStore.push(data);
+            return true;
+        }
+        return false;
+    }
+    remove(data) {
+        let pos = this.dataStore.indexOf(data);
+        if (pos < 0) {
+            this.dataStore.splice(pos, 1);
+            return true;
+        }
+        return false;
+    }
+    union(set) {
+        let tempSet = new Set();
+        for(let i in this.dataStore){
+            tempSet.add(this.dataStore[i]);
+        }
+    }
+    contains(data) {
 
-    betterHash(string) {
-        const H = 37;
-        var total = 0;
-        for (var i = 0; i < string.length; ++i) {
-            total += H * total + string.charCodeAt(i);
-        }
-        total = total % this.table.length;
-        if (total < 0) {
-            total += this.table.length - 1;
-        }
-        return parseInt(total);
-    }
-    put(data) {
-        let pos = this.betterHash(data);
-        let index = 0;
-        if (this.table[pos] == undefined) {
-            this.table[pos] = data;
-        } else {
-            while (this.table[++pos] != undefined) {}
-            this.table[pos] = data;
-        }
-    }
-
-    showDistro() {
-        for (let i in this.table) {
-            if (this.table[i] != undefined) {
-                console.log(`${i} : ${this.table[i]}`);
-            }
-            // if (this.table[i][0] != undefined) {
-            //     for (let j in this.table[i]) {
-            //         console.log(i + ": " + j + " : " + this.table[i][j]);
-            //     }
-            // }
-        }
-    }
-
-    get(key) {
-        let hash = this.betterHash(key);
-        for (let i = hash; this.table[hash] != undefined; i++) {
-            if(this.table[hash] == key) {
-                return this.table[hash];
-            }
-        }
-        return undefined;
     }
 }
-
-
-var hTable = new HashTable();
-
-var someNames = ["David", "Jennifer", "Donnie", "Raymond", "Cynthia", "Mike", "Clayton", "Danny", "Jonathan"];
-
-for (var i = 0; i < someNames.length; ++i) {
-    hTable.put(someNames[i]);
-}
-
-hTable.showDistro();
-
-console.log(hTable.get("Jonathan"))
