7691590a5caed37744b4c3a4403c9003de171557
ihongtaoi <836717428@qq.com>
2018-10-25 16:23:47 +0800

    python

--- HT/python/正则表达式.py
+++ HT/python/正则表达式.py
@@ -28,3 +28,28 @@ r3 = re.findall('a[^bfd][d][a-f]c', c);
 print(r3)
 
 # 概括字符集
+d = "asdas9asd9121__dasda  asd asdas "
+# \s \w \S \W
+
+r4 = re.findall('\s', d)
+
+print(r4)
+
+# 数量词
+
+e = 'javascript 1python12341 worid ssas'
+
+# 贪婪和非贪婪模式
+r5 = re.findall('[a-z]{3,10}', e)
+r6 = re.findall('[a-z]{3,10}?', e)
+
+print(r5)
+print(r6)
+
+
+# 边界匹配
+qq = "836717428"
+
+r7 = re.findall('^\d{4,8}$', qq)
+
+print(r7)
