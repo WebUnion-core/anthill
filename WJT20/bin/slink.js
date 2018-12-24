0ace30fd13b86a116b23175fa41dc6f87d5455a9
WeiJietao <13415156317@163.com>
2018-12-24 16:50:15 +0800

    更新slink工具，更新README.md

--- WJT20/bin/slink.js
+++ WJT20/bin/slink.js
@@ -7,21 +7,31 @@ const md5 = require('./md5');
 const srcPath = path.resolve(__dirname, './..');
 const allMdFiles = fs.readdirSync(srcPath);
 const ignore = require('./ignore.json');
-const files = [];
+const files = {};
 
 // 格式化每篇记录的标题
 function formatEachFile() {
     allMdFiles.forEach(function(item, index) {
         if (ignore.indexOf(item) < 0 && item.indexOf('.md') > 0) {
             const filePath = srcPath + '/' + item;
-
-            files.push(item);
             console.log(filePath);
 
             // isFile: 是否文件
             // isDirectory: 是否目录
             if (fs.statSync(filePath).isFile()) {
                 let fileCont = fs.readFileSync(filePath, 'utf-8');
+                const _tag = fileCont.match(/TAG        : .+/)[0].replace('TAG        : ', '');
+                const _id = fileCont.match(/ID         : .+/)[0].replace('ID         : ', '');
+
+                if (!files[_tag]) {
+                    files[_tag] = [];
+                }
+
+                files[_tag].push({
+                    name: item.replace('.md', ''),
+                    hash: md5(item, 8),
+                    id: _id
+                });
 
                 if (fileCont.indexOf('<a name="href') < 0 && fileCont.indexOf('## 目录 ##') > 0) {
                     const matchData = fileCont.match(/\#+\s.+\s\#+/g).slice(2);
@@ -84,13 +94,13 @@ function formatEachFile() {
 
 // 格式化README
 function formatReadMe() {
-    let fileCont = fs.readFileSync(srcPath + '/README.md', 'utf-8');
-    files.forEach(function(file) {
-        const name = file.replace('.md', '');
-        const id = md5(file, 8);
-        if (!fileCont.match(id)) {
-            fileCont = fileCont.replace(' : [' + name + ']', '|' + id + ' : [' + name + ']');
-        }
+    let fileCont = '\r\n# 导航 #\r\n\r\n';
+
+    Object.keys(files).forEach(function(tag) {
+        fileCont += '## ' + tag + '系列 ##\r\n\r\n';
+        files[tag].forEach(function(item) {
+            fileCont += '- [' + item.id + ', ' + item.hash + '] : [' + item.name + '](./' + item.name + '.md)\r\n\r\n';
+        });
     });
     fs.writeFileSync(srcPath + '/README.md', fileCont, 'utf-8');
 }
